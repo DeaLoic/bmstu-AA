a9d10a69f3fef74b338b160e085153aaada74410
Khetagurov Pavel <khetagurow1@yandex.ru>
2020-11-18 18:11:08 +0300

    [lab_05] Add report and fix

--- lab_05/source/main.go
+++ lab_05/source/main.go
@@ -12,18 +12,23 @@ const MAX_ELEMENT = 100
 
 var wg sync.WaitGroup
 var log []string
+var mutexLog sync.Mutex
 
 type SetTimestamp func(*queue.Element, int64)
 
 func pingPlace(id int, message string, time int64) {
-	log = append(log, fmt.Sprintf("%d     %s    %d\n", id, message, time))
+	go func() {
+		mutexLog.Lock()
+		log = append(log, fmt.Sprintf("%d     %s    %d", id, message, time))
+		mutexLog.Unlock()
+	}()
 }
 
-func doStuff() {
-	time.Sleep(time.Millisecond * 1000)
+func doStuff(milliseconds int) {
+	time.Sleep(time.Millisecond * time.Duration(milliseconds))
 }
 
-func startHandle(queueFirst *queue.Queue) {
+func startHandle(queueFirst *queue.Queue, milliseconds int) {
 	defer wg.Done()
 	for i := 0; i < MAX_ELEMENT; {
 		element := new(queue.Element)
@@ -34,11 +39,12 @@ func startHandle(queueFirst *queue.Queue) {
 		timeNow := time.Now().UnixNano()
 		queue.SetFI(element, timeNow)
 		pingPlace(element.ID, fmt.Sprintf("  Insert %d  ", 1), timeNow)
+		doStuff(milliseconds)
 		i++
 	}
 }
 
-func handleQueue(firstQueue *queue.Queue, secondQueue *queue.Queue, stLeave SetTimestamp, stIn SetTimestamp, queueNumber int) {
+func handleQueue(firstQueue *queue.Queue, secondQueue *queue.Queue, stLeave SetTimestamp, stIn SetTimestamp, queueNumber int, milliseconds int) {
 	defer wg.Done()
 	for i := 0; i < MAX_ELEMENT; {
 		firstQueue.Mu.Lock()
@@ -48,7 +54,7 @@ func handleQueue(firstQueue *queue.Queue, secondQueue *queue.Queue, stLeave SetT
 			timeNow := time.Now().UnixNano()
 			stLeave(element, timeNow)
 			pingPlace(element.ID, fmt.Sprintf("  Leave %d   ", queueNumber), timeNow)
-			doStuff()
+			doStuff(milliseconds)
 			secondQueue.Mu.Lock()
 			secondQueue.Push(element)
 			secondQueue.Mu.Unlock()
@@ -67,13 +73,13 @@ func main() {
 	answerQueue := queue.Queue{}
 
 	wg.Add(1)
-	go handleQueue(&firstQueue, &secondQueue, queue.SetFO, queue.SetSI, 1)
+	go handleQueue(&firstQueue, &secondQueue, queue.SetFO, queue.SetSI, 1, 300)
 	wg.Add(1)
-	go handleQueue(&secondQueue, &thirdQueue, queue.SetSO, queue.SetTI, 2)
+	go handleQueue(&secondQueue, &thirdQueue, queue.SetSO, queue.SetTI, 2, 600)
 	wg.Add(1)
-	go handleQueue(&thirdQueue, &answerQueue, queue.SetTO, queue.SetA, 3)
+	go handleQueue(&thirdQueue, &answerQueue, queue.SetTO, queue.SetA, 3, 300)
 	wg.Add(1)
-	go startHandle(&firstQueue)
+	go startHandle(&firstQueue, 100)
 
 	wg.Wait()
 
