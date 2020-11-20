45dd28c5948fa123318717a8863709d05bd0b516
Pavel Khetagurov <khetagurow1@yandex.ru>
2020-11-21 00:22:03 +0300

    [lab_07] Add report and source

---
+++ lab_07/source/Segment.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab_07
+{
+    class Segment : IComparable
+    {
+        public char key;
+        public int index = 0;
+        public int size = 0;
+
+        public Segment(char key, int index, int size)
+        {
+            this.key = key;
+            this.index = index;
+            this.size = size;
+        }
+
+        public int CompareTo(object second)
+        {
+            return key.CompareTo(((Segment)second).key);
+        }
+
+        public int CompareTo(char n)
+        {
+            return key.CompareTo(n);
+        }
+    }
+
+
+}
